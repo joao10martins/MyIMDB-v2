9aa98323bceded34144a64edbf82a37dd9f0bcc2
João Martins <jmartins@innovagency.com>
2019-03-06 18:04:42 +0000

    Exploring api requests

--- app/src/main/java/com/example/myimdb/NowPlayingFragment.java
+++ app/src/main/java/com/example/myimdb/NowPlayingFragment.java
@@ -6,12 +6,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NowPlayingFragment extends Fragment {
+    private ImageView mImageMovie;
+    private TextView mTitleMovie;
 
 
     public NowPlayingFragment() {
@@ -22,8 +40,42 @@ public class NowPlayingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_now_playing, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_now_playing, container, false);
+        // Views
+        mImageMovie = rootView.findViewById(R.id.movie_img_id);
+        mTitleMovie = rootView.findViewById(R.id.movie_title_id);
+
+
+
+
+
+
+
+        return rootView;
+    }
+
+
+    private void getNowPlaying(){
+        final List<Movie> NowPlaying = new ArrayList<>();
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        final String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=07d93ad59393a99fe6bc8c1b8f0de23b&language=en-US&page=1";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray objArray = response.getJSONArray("results");
+                    for (int i=0; i < objArray.length(); i++){
+                        Movie movie = new Movie()
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        })
+
     }
 
 }
