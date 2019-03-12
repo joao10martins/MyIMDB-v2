e49ab9efebaa6d609b3488c4aa2f8fbb024428c3
João Martins <jmartins@innovagency.com>
2019-03-12 18:00:01 +0000

    Started implementation of 'Details'
    - TODO: finish layout and start coding

---
+++ app/src/main/java/com/example/myimdb/DetailsFragment.java
@@ -0,0 +1,32 @@
+package com.example.myimdb;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailsFragment extends Fragment {
+
+    private View mView;
+
+    public DetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_details, container, false);
+
+        return mView;
+    }
+
+}
