46e8c86f18a2217b380d464fd8a79ff4f0819866
João Martins <jmartins@innovagency.com>
2019-04-05 17:50:25 +0100

    Changing code to work with the new Search models

--- app/src/main/java/com/example/myimdb/map/MovieMapper.java
+++ app/src/main/java/com/example/myimdb/map/MovieMapper.java
@@ -3,12 +3,14 @@ package com.example.myimdb.map;
 
 import com.example.myimdb.model.Movie;
 import com.example.myimdb.model.MovieRealm;
+import com.example.myimdb.model.SearchMovie;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MovieMapper {
     List<MovieRealm> myMovieRealmList = new ArrayList<>();
+    List<SearchMovieRealm> mySearchRealmList = new ArrayList<>();
 
     public List<MovieRealm> toMovieRealmList(List<Movie> responseList){
         for (Movie movie : responseList){
@@ -29,6 +31,19 @@ public class MovieMapper {
 
 
     // TODO:
-    // create funtion to parse SearchMovie to SearchMovieRealm
+    // create funtion to parse SearchMovie to SearchMovieRealm(need to create RealmModels)
+    public List<SearchMovieRealm> toSearchRealmList(List<SearchMovie> responseList){
+        for (SearchMovie movie : responseList){
+            SearchMovieRealm mySearchRealm = new SearchMovieRealm(
+                    movie.getId(),
+                    movie.getTitle(),
+                    movie.getPoster_path(),
+                    //movie.getGenre_ids(), //error here (types)
+                    movie.getRelease_date(),
+                    movie.getGenresDescription());
+            mySearchRealmList.add(mySearchRealm);
+        }
+        return mySearchRealmList;
+    }
 }
 
