a2e009904062ba6efee42e974de2e67f33591850
João Martins <jmartins@innovagency.com>
2019-05-02 17:40:27 +0100

    Creating a Login feature

---
+++ app/src/main/java/com/example/myimdb/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.myimdb;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+
+import com.example.myimdb.helpers.BlurBuilder;
+
+import jp.wasabeef.blurry.Blurry;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+
+
+
+    }
+}
